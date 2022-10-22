79df19d571a15e2bde2f23e045601d99d5149e95
Yulik <yulikpoch@gmail.com>
2022-10-22 16:50:22 +0200

    разбивка на страницы и компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  CardsCount : 312,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      cardsCount = {Setting.CardsCount}
+    />
   </React.StrictMode>,
 );
